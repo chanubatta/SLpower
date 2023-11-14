b7949cf5669e0e9b8bb497e17ced806507cedd3c
chanubatta <72717444+chanubatta@users.noreply.github.com>
2023-11-14 07:26:39 +0530

    gg.py

--- gg.py
+++ gg.py
@@ -1,6 +1,7 @@
 #WRITEN BY MR SL CHANUKA
 #MY GITHUB :http://github.com/chanubatta
 #--------import-------#
+from os import system as clr
 #--------logo-------#
 logo=("""
 ██████  ██▓        ██░ ██  ▄▄▄       ▄████▄   ██ ▄█▀▓█████  ██▀███  
@@ -15,10 +16,19 @@ logo=("""
                                       ░                               """)
 #-------clear-------#
 def clear():
+	clr('clear')
 	print(logo)
-	print(40*'-')
-	print('Facebook : Chanuka Ravindu')
+	print(45*'-')
+	print('SL Power Gang']
+	print('Facebook :www.facebook.com\Chanuka.Ravindu.12')
+	print('whatsapp number : +94740074336')
+	print('GitHub : SL Power Gang')
 	print(40*'-')
 #--------main------#
+def main():
+	clear()
+	print('[01]Facebook Hack') 
+	print('[02]Exit')
+	option=input('[CHOICE MENU : ')
 #--------end--------#
 clear()
